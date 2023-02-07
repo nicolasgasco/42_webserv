5aea3dc2dd0add8f811ae9d2be73cc28d3196e50
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-02-07 22:15:13 +0100

    Add tests for HttpRequestClass

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,7 @@
 #include "classes/address_info_class_tests.cpp"
 #include "classes/socket_class_tests.cpp"
 #include "classes/socket_connection_class_tests.cpp"
+#include "classes/http_request_class_tests.cpp"
 #include "utils/ltrim_tests.cpp"
 #include "utils/rtrim_tests.cpp"
 #include "utils/trim_tests.cpp"
@@ -13,12 +14,12 @@ int main(int argc, char *argv[])
     if (argc > 1)
         IS_DEBUG = (static_cast<std::string>(argv[1]) == "debug") ? true : false;
 
-    address_info_class_tests(IS_DEBUG);
-    socket_class_tests(IS_DEBUG);
-    socket_connection_class_tests(IS_DEBUG);
-
     ltrim_tests(IS_DEBUG);
     rtrim_tests(IS_DEBUG);
     trim_tests(IS_DEBUG);
 
+    address_info_class_tests(IS_DEBUG);
+    socket_class_tests(IS_DEBUG);
+    socket_connection_class_tests(IS_DEBUG);
+    http_request_class_tests(IS_DEBUG);
 }
