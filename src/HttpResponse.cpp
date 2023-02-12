175ddd49962b229d77bf13ec8dd3dd662c17f4a6
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-02-12 18:19:41 +0100

    Fetch files from public folders in HttpResponse class

--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -1,13 +1,15 @@
 #include "HttpResponse.hpp"
 
-HttpResponse::HttpResponse(int &code, std::string &reason)
+HttpResponse::HttpResponse(HttpRequest &req)
 {
     _status_line.version = HTTP_PROTOCOL;
-    _status_line.code = code;
-    _status_line.reason = reason;
+    // TODO compute this instead of mocking
+    _status_line.code = req.has_error() ? req.gett_err().code : 200;
+    // TODO compute this instead of mocking
+    _status_line.reason = req.has_error() ? req.gett_err().message : "OK";
 
     this->_build_status_line();
-    this->_build_message_body();
+    this->_build_message_body(req.get_req_line().target);
 
     // TODO delete this when build is done
     std::cout << *this << std::endl;
@@ -35,10 +37,21 @@ void HttpResponse::_build_status_line()
     this->_buff += this->_status_line.reason + "\r\n";
 }
 
-void HttpResponse::_build_message_body()
+void HttpResponse::_build_message_body(std::string target)
 {
-    // TODO add logic to fetch file dynamically
-    std::ifstream file("public/index.html");
+    std::string file_path;
+    if (target == "/")
+    {
+        file_path += PUBLIC_PATH;
+        file_path += "/index.html";
+    }
+    else
+    {
+        // TODO add logic to fetch file dynamically
+        file_path = PUBLIC_PATH + target;
+    }
+
+    std::ifstream file(file_path);
     std::string buffer_str;
 
     if (file)
@@ -47,6 +60,11 @@ void HttpResponse::_build_message_body()
         data_stream << file.rdbuf(); // reading data
         this->_buff += "\r\n";
         this->_buff += data_stream.str();
+    }
+    else
+    {
+        // TODO file doesn't exist
+        std::cerr << RED << "File does not exist..." << NC << std::endl;
     }
 }
 
@@ -60,9 +78,6 @@ std::ostream &operator<<(std::ostream &os, HttpResponse &std)
     std::cout << "reason-phrase: " << YELLOW << "." << NC << std.get_status_line().reason << YELLOW << "." << NC << std::endl;
 
     std::cout << std::endl;
-
-    std::cout << "BODY:" << std::endl;
-    std::cout << std.get_buff() << std::endl;
 
     return os;
 }
