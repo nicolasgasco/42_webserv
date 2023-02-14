0860ca31647ed8ab2967e53ccbeed15143a831d7
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-02-14 18:33:59 +0100

    Make util functions accept only const references

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <string>
 
-std::string ltrim(std::string &str);
-std::string rtrim(std::string &str);
-std::string trim(std::string &str);
-bool str_isspace(std::string &str);
+std::string ltrim(std::string const &str);
+std::string rtrim(std::string const &str);
+std::string trim(std::string const &str);
+bool str_isspace(std::string const &str);
 bool str_ends_with(std::string const &value, std::string const &ending);
 
-std::string build_path(std::string frag1, std::string frag2);
-std::string build_path(std::string frag1, std::string frag2, std::string frag3);
+std::string build_path(std::string const &frag1, std::string const &frag2);
+std::string build_path(std::string const &frag1, std::string const &frag2, std::string const &frag3);
