8b92a8ac7b9ba5b437de89704718f62ea57d3ce2
Diego Gerwig <dgerwig-@student.42urduliz.com>
2023-02-15 21:14:15 +0100

    Working on parser config_file

---
+++ inc/Configuration.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class Config
+{
+	public:
+		Config();
+		~Config();
+    
+		void  	load_configuration(char* config_file);
+    	void 	validate_configuration();
+
+
+	private:
+
+
+
+};
---
+++ src/Configuration.cpp
@@ -0,0 +1,15 @@
+#include "Configuration.hpp"
+
+Config::Config() {}
+
+Config::~Config() {}
+
+
+void  	Config::load_configuration(char* config_file) 
+{
+	(void)config_file;
+
+}
+
+
+void 	Config::validate_configuration() {}
