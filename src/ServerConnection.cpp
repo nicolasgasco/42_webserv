b9105ff4d025f23fb07ccfeee003350896db8845
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-02-19 15:29:45 +0100

    Refactor ServerConnection logic to be more declarative

--- src/ServerConnection.cpp
+++ src/ServerConnection.cpp
@@ -1,26 +1,7 @@
 #include "ServerConnection.hpp"
 
-ServerConnection::ServerConnection(int const &sock_id, addrinfo *addr_info, RouterService const &router)
+ServerConnection::ServerConnection()
 {
-    socklen_t addr_info_size = sizeof addr_info;
-    this->_new_sock_id = accept(sock_id, (struct sockaddr *)addr_info, &addr_info_size);
-    this->_check_accept_return();
-
-    HttpRequest req;
-
-    char buff[REC_BUFF_SIZE];
-    this->_bytes_received = recv(this->_new_sock_id, (void *)buff, REC_BUFF_SIZE, 0);
-    this->_check_recv_return();
-
-    req.set_buff(buff);
-
-    req.parse_req();
-    req.output_status();
-
-    HttpResponse res(req, router);
-
-    this->_bytes_sent = send(this->_new_sock_id, res.get_buff().c_str(), res.get_buff().length(), 0);
-    this->_check_send_return();
 }
 
 ServerConnection::~ServerConnection()
@@ -29,13 +10,11 @@ ServerConnection::~ServerConnection()
     close(this->_new_sock_id);
 }
 
-int ServerConnection::get_new_sock_id() const
+void ServerConnection::accept_connection(int const &sock_id, addrinfo *addr_info)
 {
-    return this->_new_sock_id;
-}
+    socklen_t addr_info_size = sizeof addr_info;
+    this->_new_sock_id = accept(sock_id, (struct sockaddr *)addr_info, &addr_info_size);
 
-void ServerConnection::_check_accept_return() const
-{
     if (this->_new_sock_id == -1)
         std::cerr << "Error: accept: " << std::strerror(errno) << std::endl;
     else
@@ -43,18 +22,42 @@ void ServerConnection::_check_accept_return() const
                   << "New connection accepted (" << this->_new_sock_id << ")..." << NC << std::endl;
 }
 
-void ServerConnection::_check_recv_return() const
+void ServerConnection::handle_connection(RouterService const &router)
 {
+    HttpRequest req;
+
+    this->_receive_req(req);
+
+    this->_send_res(req, router);
+}
+
+void ServerConnection::_receive_req(HttpRequest &req)
+{
+    char buff[REC_BUFF_SIZE];
+    this->_bytes_received = recv(this->_new_sock_id, (void *)buff, REC_BUFF_SIZE, 0);
     if (this->_bytes_received == -1)
         std::cerr << "Error: recv: " << std::strerror(errno) << std::endl;
     else
         std::cout << YELLOW << "Bytes received: " << this->_bytes_received << NC << std::endl;
+
+    req.set_buff(buff);
+
+    req.parse_req();
+    req.output_status();
 }
 
-void ServerConnection::_check_send_return() const
+void ServerConnection::_send_res(HttpRequest &req, RouterService const &router)
 {
+    HttpResponse res(req, router);
+
+    this->_bytes_sent = send(this->_new_sock_id, res.get_buff().c_str(), res.get_buff().length(), 0);
     if (this->_bytes_sent == -1)
         std::cerr << "Error: send: " << std::strerror(errno) << std::endl;
     else
         std::cout << YELLOW << "Bytes sent: " << this->_bytes_sent << NC << std::endl;
+}
+
+int const &ServerConnection::get_new_sock_id() const
+{
+    return this->_new_sock_id;
 }
