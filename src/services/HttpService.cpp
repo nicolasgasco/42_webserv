72fbc455b04f662114751638a8be2d4bdbca37c8
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-03-06 21:37:24 +0100

    Add comments to HttpService

--- src/services/HttpService.cpp
+++ src/services/HttpService.cpp
@@ -10,6 +10,10 @@ HttpService::~HttpService()
 {
 }
 
+/**
+ * Writes file into std::string.
+ * @param file File stream.
+ */
 std::string const HttpService::build_file(std::ifstream const &file) const
 {
     std::string message_body;
@@ -24,25 +28,30 @@ std::string const HttpService::build_file(std::ifstream const &file) const
     return message_body;
 }
 
+/**
+ * Build headers for a response.
+ * @param content_len Len of content to be sent.
+ */
 std::string const HttpService::build_headers(int const &content_len, class Webserver *webserver) const
 {
     std::string headers;
 
     std::string date = "Date: " + get_gmt_time() + "\r\n";
 
-	std::string server_name;
+    std::string server_name;
 
     for (std::vector<Server>::iterator it = webserver->_server.begin(); it != webserver->_server.end(); it++)
-	{
+    {
         Server srv_data = *it;
         server_name = srv_data.get_server_name();
-		std::cout << "Server: " << std::string(server_name) << std::endl;
-	}
-    
-	std::string server = "Server: " + std::string(server_name) + "/1.0" + "\r\n";
+        std::cout << "Server: " << std::string(server_name) << std::endl;
+    }
+
+    std::string server = "Server: " + std::string(server_name) + "/1.0" + "\r\n";
 
     std::string content_length = "Content-Length: " + std::to_string(content_len) + "\r\n";
 
+    // TODO check if Content-Type is creating issues
     // std::string content_type = this->_get_content_type(this->_req.get_req_line().target);
 
     headers += date;
@@ -53,6 +62,12 @@ std::string const HttpService::build_headers(int const &content_len, class Webse
     return headers;
 }
 
+/**
+ * Writes a formatted status line.
+ * @param version Response HTTP version.
+ * @param code Response code.
+ * @param reason Response message.
+ */
 std::string const HttpService::build_status_line(std::string const &version, int const &code, std::string const &reason) const
 {
     std::string status_line;
