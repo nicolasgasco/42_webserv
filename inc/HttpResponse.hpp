8b0e987e15b6b2dba66859cd70f38386a38860c1
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-03-06 21:57:23 +0100

    Add comments to HttpResponse class

--- inc/HttpResponse.hpp
+++ inc/HttpResponse.hpp
@@ -23,37 +23,39 @@ struct StatusLine
 class HttpResponse
 {
 private:
-    std::string _buff;
-    StatusLine _status_line;
     RouterService const &_router;
     HttpService _http;
     CgiService _cgi;
+    Webserver _server;
+
+    std::string _buff;
     HttpRequest _req;
-	Webserver _server;
+    StatusLine _status_line;
 
 public:
     HttpResponse(RouterService const &router, class Webserver *webserver);
     ~HttpResponse();
 
     // Getters
-    StatusLine const &get_status_line() const;
     std::string const &get_buff() const;
+    StatusLine const &get_status_line() const;
 
     // Setters
-    void set_status_line(int const &code, std::string const &reason);
     void set_buff(std::string const &buff);
+    void set_status_line(int const &code, std::string const &reason);
 
     // Methods
     void build_response(HttpRequest req, HttpService const &http, CgiService const &cgi, class Webserver *webserver);
     void reset();
 
 private:
-    void _build_error_res(class Webserver *webserver);
-    void _build_get_res(class Webserver *webserver);
-    void _build_post_res(class Webserver *webserver);
     void _build_delete_res(class Webserver *webserver);
+    void _build_get_res(class Webserver *webserver);
+    void _build_error_res(class Webserver *webserver);
+    void _build_post_res(class Webserver *webserver);
 
     void _replace_var_in_page(std::string &file, std::string const var, std::string const value) const;
+
     std::string const _get_content_type(std::string target) const;
 
 public:
