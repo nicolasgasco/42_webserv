04b4744b8f71186540e76fe5528eca52c39510ff
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2023-03-11 19:51:51 +0100

    Revert "Pair session: bind each server's socket"

--- inc/AddressInfo.hpp
+++ inc/AddressInfo.hpp
@@ -47,13 +47,13 @@ private:
     struct addrinfo *_serv_info;
 
 public:
-    AddressInfo(std::string const &port, std::string const &host_name);
+    AddressInfo(std::string const &port, class Webserver *webserver);
     ~AddressInfo();
 
     struct addrinfo *get_serv_info() const;
 
 private:
-    void _fill_addr_info(std::string const &port, std::string const &host_name);
+    void _fill_addr_info(std::string const &port, class Webserver *webserver);
     struct addrinfo _fill_hints();
     void _check_addr_info_status(int const &status) const;
 };
