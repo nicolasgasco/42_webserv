b9b4a09f0f0cf9db6a02b2741c33b2f0d0a9102b
Diego Gerwig <dgerwig-@student.42urduliz.com>
2023-03-12 18:28:39 +0100

    Parsing alias path

--- inc/Webserver.hpp
+++ inc/Webserver.hpp
@@ -16,11 +16,10 @@ class Webserver
     	Webserver();
     	~Webserver();
 
-    	void   	load_config_file(char *config_file);
-    	void    print_config_data();
-		void	inspect_config_data();
-//		std::string	bind_socket(std::string *port);
-		std::vector<Server> &get_server();
+    	void   					load_config_file(char *config_file);
+    	void    				print_config_data();
+		void					inspect_config_data();
+		std::vector<Server> 	&get_server();
 
 	private:
     	std::vector<Server>     _server;
