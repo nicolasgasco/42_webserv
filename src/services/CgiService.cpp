0df3e84438f89c97e3c12f60e133315512642785
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-03-12 11:45:20 +0100

    Add env variables to CGI scripts

--- src/services/CgiService.cpp
+++ src/services/CgiService.cpp
@@ -38,3 +38,57 @@ std::string const CgiService::build_cgi_output(char *const *args, char *const *e
 
     return std::string(buff);
 }
+
+std::vector<std::string> CgiService::build_envp(std::string path, HttpRequest const &req) const
+{
+    std::vector<std::string> envp;
+
+    std::string server_protocol = "SERVER_PROTOCOL=" + req.get_req_line().version;
+    envp.push_back(server_protocol);
+
+    // TODO change with real server port
+    std::string server_port = "SERVER_PORT=" + std::to_string(8180);
+    envp.push_back(server_port);
+
+    std::string request_method = "REQUEST_METHOD=" + req.get_req_line().method;
+    envp.push_back(request_method);
+
+    std::string path_info = "PATH_INFO=./" + path;
+    envp.push_back(path_info);
+
+    try
+    {
+        if (req.get_attrs().at("Host").length())
+        {
+            std::string remote_host = "REMOTE_HOST=" + req.get_attrs().at("Host");
+            envp.push_back(const_cast<char *>(remote_host.c_str()));
+        }
+    }
+    catch (std::out_of_range &e)
+    {
+    }
+
+    // Remote adress
+
+    // Content Type
+    try
+    {
+        std::string content_type = "CONTENT_TYPE=" + req.get_attrs().at("Content-Type");
+        envp.push_back(content_type);
+    }
+    catch (std::out_of_range &e)
+    {
+    }
+
+    // Content Length
+    try
+    {
+        std::string content_length = "CONTENT_LENGTH=" + req.get_attrs().at("Content-Length");
+        envp.push_back(content_length);
+    }
+    catch (std::out_of_range &e)
+    {
+    }
+
+    return envp;
+}
