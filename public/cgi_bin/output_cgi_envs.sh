de43b727524fb994b7ecf8980bce499d89ac15d6
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-03-15 21:58:22 +0100

    Reorder CGI env variables

--- public/cgi_bin/output_cgi_envs.sh
+++ public/cgi_bin/output_cgi_envs.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+vars=""
+
 # HTML-formatted output of CGI environment variables
-vars="<li><b>SERVER_SOFTWARE</b>=$SERVER_SOFTWARE</li>"
+vars+="<li><b>CONTENT_LENGTH</b>=$CONTENT_LENGTH</li>"
+vars+="<li><b>CONTENT_TYPE</b>=$CONTENT_TYPE</li>"
+vars+="<li><b>GATEWAY_INTERFACE</b>=$GATEWAY_INTERFACE</li>"
+vars+="<li><b>PATH_INFO</b>=$PATH_INFO</li>"
+vars+="<li><b>PATH_TRANSLATED</b>=$PATH_TRANSLATED</li>"
+vars+="<li><b>QUERY_STRING</b>=$QUERY_STRING</li>"
+vars+="<li><b>REMOTE_ADDR</b>=$REMOTE_ADDR</li>"
+vars+="<li><b>REQUEST_METHOD</b>=$REQUEST_METHOD</li>"
+vars+="<li><b>SCRIPT_NAME</b>=$SCRIPT_NAME</li>"
 if [[ $SERVER_NAME ]]; then
     vars+="<li><b>SERVER_NAME</b>=$SERVER_NAME</li>"
 fi
-vars+="<li><b>GATEWAY_INTERFACE</b>=$GATEWAY_INTERFACE</li>"
-vars+="<li><b>SERVER_PROTOCOL</b>=$SERVER_PROTOCOL</li>"
 vars+="<li><b>SERVER_PORT</b>=$SERVER_PORT</li>"
-vars+="<li><b>REQUEST_METHOD</b>=$REQUEST_METHOD</li>"
-vars+="<li><b>PATH_INFO</b>=$PATH_INFO</li>"
-vars+="<li><b>SCRIPT_NAME</b>=$SCRIPT_NAME</li>"
-if [[ $QUERY_STRING ]]; then
-    vars+="<li><b>QUERY_STRING</b>=$QUERY_STRING</li>"
-fi
-if [[ $CONTENT_TYPE ]]; then
-    vars+="<li><b>CONTENT_TYPE</b>=$CONTENT_TYPE</li>"
-fi
-if [[ $CONTENT_LENGTH ]]; then
-    vars+="<li><b>CONTENT_LENGTH</b>=$CONTENT_LENGTH</li>"
-fi
+vars+="<li><b>SERVER_PROTOCOL</b>=$SERVER_PROTOCOL</li>"
+vars+="<li><b>SERVER_SOFTWARE</b>=$SERVER_SOFTWARE</li>"
 
 # Read in index.html and replace {{vars}} with $vars
 page=""
