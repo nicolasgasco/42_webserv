77434821bdcd49e89e4036957cd22c188500a4d3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-04-12 00:13:52 +0200

    Make DELETE request logic more robust

--- cgi-bin/delete_file.py
+++ cgi-bin/delete_file.py
@@ -8,7 +8,17 @@ path = os.getenv("PATH_TRANSLATED")
 try:
     os.remove(path)
     result = {"result": "Success", "status": 200, "path": path}
+
+    print("HTTP/1.1 200 OK\r")
+    print("Content-Type: application/json\r")
+    print("Content-Length: " + str(len(json.dumps(result, indent=4))) + "\r")
+    print("\r")
+
+    print(json.dumps(result, indent=4))
+
 except OSError:
     result = {"result": "File does not exist", "status": 404, "path": path}
-
-print(json.dumps(result, indent=4))
+    print("HTTP/1.1 404 Not Found\r")
+    print("Content-Type: application/json\r")
+    print("Content-Length: " + str(len(json.dumps(result, indent=4))) + "\r")
+    print("\r")
