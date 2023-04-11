77a6cade2e7afb2cee1ee5ae9f9bed9ac86f143a
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-04-11 23:53:09 +0200

    Get all POST request output from CGI script

--- cgi-bin/save_picture.py
+++ cgi-bin/save_picture.py
@@ -3,26 +3,47 @@ import os
 import os.path
 import sys
 
-form = cgi.FieldStorage()
+try:
+    form = cgi.FieldStorage()
 
-# Get filename here
-fileitem = form['image']
+    # Get filename here
+    fileitem = form['image']
 
-# Test if the file was uploaded
-if fileitem.filename:
-    file_path = os.getcwd() + '/public/gallery/pictures/' + \
-        os.path.basename(fileitem.filename)
+    # Test if the file was uploaded
+    if fileitem.filename:
+        file_path = os.getcwd() + '/public/gallery/pictures/' + \
+            os.path.basename(fileitem.filename)
 
-    # If file already exists, return 409 Conflict
-    if os.path.isfile(file_path):
-        with open('./public/error/default.html', 'r') as file:
-            error_page = file.read()
-            error_page = error_page.replace("{{code}}", "409")
-            error_page = error_page.replace("{{message}}", "Conflict")
-            print(error_page)
-        sys.exit(0)
+        # If file already exists, return 409 Conflict
+        if os.path.isfile(file_path):
+            with open('./public/error/default.html', 'r') as file:
+                error_page = file.read()
+                error_page = error_page.replace("{{code}}", "409")
+                error_page = error_page.replace("{{message}}", "Conflict")
+                
+                print ("HTTP/1.1 409 Conflict\r")
+                print("Content-Length: " + str(len(error_page)) + "\r")
+                print("Content-Type: text/html\r")
+                print("\r\n")
+                print(error_page)
+            sys.exit(0)
 
-    open(file_path, 'wb').write(fileitem.file.read())
-    with open('./public/gallery/upload/success.html', 'r') as file:
-        success_page = file.read()
-        print(success_page)
+        open(file_path, 'wb').write(fileitem.file.read())
+        with open('./public/gallery/upload/success.html', 'r') as file:
+            success_page = file.read()
+            print("HTTP/1.1 200 OK\r")
+            print("Content-Length: " + str(len(success_page)) + "\r")
+            print("Content-Type: text/html\r")
+            print("\r")
+            print(success_page)
+except:
+    with open('./public/error/default.html', 'r') as file:
+        error_page = file.read()
+        error_page = error_page.replace("{{code}}", "500")
+        error_page = error_page.replace("{{message}}", "Internal Server Error")
+
+    print ("HTTP/1.1 500 Internal Server Error\r")
+    print("Content-Length: " + str(len(error_page)) + "\r")
+    print("Content-Type: text/html\r")
+    print("\r\n")
+    print(error_page)  
